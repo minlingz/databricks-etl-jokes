994ebef4a2357dd4677fa7606bbdce292c5fa756
minlingz <minling.zhou@gmail.com>
2023-11-15 21:25:38 -0500

    add readme

--- etl_databricks_jokes.py
+++ etl_databricks_jokes.py
@@ -4,7 +4,9 @@ from pyspark.sql.functions import col, current_timestamp, explode, split, regexp
 from pyspark.sql.types import *
 
 # Define variables used in code below
-username = spark.sql("SELECT regexp_replace(current_user(), '[^a-zA-Z0-9]', '_')").first()[0]
+username = spark.sql(
+    "SELECT regexp_replace(current_user(), '[^a-zA-Z0-9]', '_')"
+).first()[0]
 table_name = f"{username}_etl_jokes"
 checkpoint_path = f"/tmp/{username}/_checkpoint/etl_jokes"
 file_location = "dbfs:/FileStore/shared_uploads/mz246@duke.edu/jokes/"
@@ -14,23 +16,41 @@ spark.sql(f"DROP TABLE IF EXISTS {table_name}")
 dbutils.fs.rm(checkpoint_path, True)
 
 # Configure Auto Loader to ingest JSON data to a Delta table
-(spark.readStream
-  .format("cloudFiles")
-  .option("cloudFiles.format", "json")
-  .option("cloudFiles.schemaLocation", checkpoint_path)
-  .load(file_location)
-  .withColumn("ratingCount", col("ratingCount").cast(IntegerType()))
-  .withColumn("ratingValue", col("ratingValue").cast(DoubleType()))
-  .select("*", explode(split(col("tags"), ",")).alias("tag_raw"))
-  .withColumn("tag", regexp_extract(col("tag_raw"), '\\"(.*)\\"', 1))
-  .select("*", col("_metadata.file_path").alias("source_file"), current_timestamp().alias("processing_time"))
-  .drop("tag_raw")
-  .writeStream
-  .option("checkpointLocation", checkpoint_path)
-  .trigger(availableNow=True)
-  .toTable(table_name))
+(
+    spark.readStream.format("cloudFiles")  # use Auto Loader
+    .option("cloudFiles.format", "json")  # read JSON files
+    .option(
+        "cloudFiles.schemaLocation", checkpoint_path
+    )  # use checkpoint location to store schema
+    .load(file_location)  # read files from file_location
+    .withColumn(
+        "ratingCount", col("ratingCount").cast(IntegerType())
+    )  # cast ratingCount to integer
+    .withColumn(
+        "ratingValue", col("ratingValue").cast(DoubleType())
+    )  # cast ratingValue to double
+    .select(
+        "*", explode(split(col("tags"), ",")).alias("tag_raw")
+    )  # explode tags into separate rows for each tag
+    .withColumn(
+        "tag", regexp_extract(col("tag_raw"), '\\"(.*)\\"', 1)
+    )  # extract tag from tag_raw
+    .select(
+        "*",
+        col("_metadata.file_path").alias(
+            "source_file"
+        ),  # add source file name from metadata
+        current_timestamp().alias("processing_time"),  # add processing time
+    )
+    .drop("tag_raw")  # drop tag_raw column which is no longer needed
+    .writeStream.option(
+        "checkpointLocation", checkpoint_path
+    )  # checkpoint to avoid reprocessing files
+    .trigger(availableNow=True)  # process files as soon as they appear
+    .toTable(table_name)  # write to Delta table
+)
 
 # COMMAND ----------
 
-df = spark.read.table(table_name)
-display(df)
+df = spark.read.table(table_name)  # read from Delta table
+display(df)  # display data
